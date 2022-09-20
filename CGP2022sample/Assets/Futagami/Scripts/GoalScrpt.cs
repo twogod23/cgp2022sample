f3dc34fd3eb26a6193ff58b039ea59366f5db0c4
twogod23 <60266261.futagami.cgp@gmail.com>
2022-09-20 12:19:05 +0900

    2002209201219

--- CGP2022sample/Assets/Futagami/Scripts/GoalScrpt.cs
+++ CGP2022sample/Assets/Futagami/Scripts/GoalScrpt.cs
@@ -1,18 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GoalScrpt : MonoBehaviour
 {
+    public static float goaltime;
+    public GameObject goaltimeTxt;
+    private bool goal;
+    private float loadSceneTime;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        goaltime = 0.0f;
+        loadSceneTime = 2.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (goal == true)
+        {
+            loadSceneTime -= Time.deltaTime;
+
+            if (loadSceneTime <= 0)
+            {
+                //SceneManager.LoadScene("GoalScene");
+                Debug.Log("goaltime");
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (goal == false)
+        {
+            if (other.gameObject.CompareTag("GoalBlockSurface"))
+            {
+                goaltime = CountDownTimeScript.GetTime();
+                Destroy(goaltimeTxt);
+                goal = true;
+            }
+        }
+    }
+
+    public static float GetGoaltime()
+    {
+        return goaltime;
     }
 }
